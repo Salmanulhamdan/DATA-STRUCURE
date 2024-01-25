b230978fc3d925b11dc6d51ce75b30e2f1d7d959
salmanulhamdan <107472231+Salmanulhamdan@users.noreply.github.com>
2024-01-25 15:14:05 +0000

    bubblesort

--- bubblesort.py
+++ bubblesort.py
@@ -1,4 +1,5 @@
 #bubblesort1
+print("\r")
 arr=[3.14, 2.71, 1.41, 1.73, 2.24, 1.62]
 for i in range(len(arr)):
     for j in range(len(arr)-1):
@@ -6,3 +7,16 @@ for i in range(len(arr)):
             arr[j],arr[j+1]=arr[j+1],arr[j]
 
 print (arr)
+
+print("\r")
+for j in range(10):
+    print ('*'*j)
+print("\r")
+print("\r")
+
+
+for i in range(0,10):
+    for j in range(0,i+1):
+          print("* ",end="")
+    print("\r")
+    
